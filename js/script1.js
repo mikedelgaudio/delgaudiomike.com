68222ff913928e3c453503e8d10ffd5bd5ac35c6
Mike DelGaudio <mikedelgaudio@Mikes-MacBook-Pro.local>
2018-09-26 18:20:16 -0400

    Adding live websiteas of 09-26-18.

---
+++ js/script1.js
@@ -0,0 +1,7 @@
+// JavaScript Document
+
+document.getElementById("buildNumber").innerHTML = "v1.4";
+
+function gameAlert() {
+    alert("This page is coming soon. Thank you for your patience.");
+}
