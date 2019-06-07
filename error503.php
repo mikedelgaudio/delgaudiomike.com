d2bad0adef9af498dd06c7e848214dbcec59d1df
Mike DelGaudio <delgaudiomike@gmail.com>
2019-06-07 12:50:02 -0400

    added custom error 503 page
    Changed version number.

---
+++ error503.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-119776312-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-119776312-1');
+</script>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<meta name="robots" content="noindex">
+	<meta name="googlebot" content="noindex">
+	<meta name="title" content="Mike DelGaudio">
+	<meta property="og:site_name" content="Mike DelGaudio">
+	<meta name="author" content="Michael DelGaudio">
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<title>Error 404</title>
+<!--favicon-->
+<link rel="shortcut icon" type="image/ico" href="images/favicon.ico"/>
+<!--StyleSheets-->
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+<link rel="stylesheet" type="text/css" href="css/indexStyle.css"/>
+<link rel="stylesheet" type="text/css" href="css/main.css"/>
+</head>
+<body>
+	<div class="backgroundIndex">
+		<div class="w3-animate-zoom">
+			<div id="container">
+  				<a href="/"><p>Error 503</p></a>
+			</div>
+			<p id="indexText">Something is wrong on our end. Try again soon!</p>
+		</div>
+	<br>
+		<p style="font-size: 1em;"><a href="/" rel="nofollow"><button class="w3-button w3-white w3-round-large">Home</button></a></p>
+	</div>
+<script src="js/logger.js"></script>
+</body>
+</html>
--- js/logger.js
+++ js/logger.js
@@ -1,3 +1,2 @@
-console.log("version_1.9");
-console.log("updated_05_27_2019");
-console.log("Summer Update 2019");
+console.log("version_1.9.1");
+console.log("updated_06_07_2019");
