24a299ac8e1add04fc90a545f6fd782fedf6d2b3
Mike DelGaudio <delgaudiomike@gmail.com>
2019-08-17 23:05:21 -0400

    added noreferrer

--- contact.php
+++ contact.php
@@ -25,18 +25,18 @@ include_once('header.php');
         <div id="contactMeModal" class="w3-modal">
             <div class="w3-modal-content">
               <div class="w3-container w3sContainerBackground">
-                <h5>If you would like to ask any questions or request a resume, please <a class="contactLink" href="mailto:delgaudiomike@gmail.com" rel="nofollow">contact me</a>. Additionally, leave me some <a class="contactLink" href="mailto:delgaudiomike@gmail.com" rel="nofollow">feedback</a>!</h5>
+                <h5>If you would like to ask any questions or request a resume, please <a class="contactLink" href="mailto:delgaudiomike@gmail.com" rel="noreferrer nofollow">contact me</a>. Additionally, leave me some <a class="contactLink" href="mailto:delgaudiomike@gmail.com" rel="noreferrer nofollow">feedback</a>!</h5>
               </div>
                 <div class="w3-container w3sContainerTitle">
                       <div class="spacer20"></div>
                         <div class="centerIcons">
-                          <p class="contactIcons socialIcons"><a href="mailto:delgaudiomike@gmail.com" rel="nofollow"><button class="w3-button w3-black w3-round-large highlighter" title="E-Mail Me"><img src="images/emailIcon.png" alt="email me"></button></a></p>
+                          <p class="contactIcons socialIcons"><a href="mailto:delgaudiomike@gmail.com" rel="noreferrer nofollow"><button class="w3-button w3-black w3-round-large highlighter" title="E-Mail Me"><img src="images/emailIcon.png" alt="email me"></button></a></p>
                   
-                          <p class="contactIcons socialIcons"><a href="https://www.linkedin.com/in/michael-delgaudio/" rel="nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="LinkedIn"><img src="images/linkedIN.png" alt="LinkedIn"></button></a></p>
+                          <p class="contactIcons socialIcons"><a href="https://www.linkedin.com/in/michael-delgaudio/" rel="noreferrer nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="LinkedIn"><img src="images/linkedIN.png" alt="LinkedIn"></button></a></p>
                       
-                          <p class="contactIcons socialIcons"><a href="https://www.youtube.com/channel/UCbOZil6oma5l5HO-LZbOEUQ?sub_confirmation=1/" rel="nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="YouTube"><img src="images/youtube.png" alt="YouTube"></button></a></p>
+                          <p class="contactIcons socialIcons"><a href="https://www.youtube.com/channel/UCbOZil6oma5l5HO-LZbOEUQ?sub_confirmation=1/" rel="noreferrer nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="YouTube"><img src="images/youtube.png" alt="YouTube"></button></a></p>
                   
-                          <p class="contactIcons socialIcons"><a href="https://github.com/mikedelgaudio" rel="nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="Github"><img src="images/gitHub.png" alt="GitHub"></button></a></p>
+                          <p class="contactIcons socialIcons"><a href="https://github.com/mikedelgaudio" rel="noreferrer nofollow" target="_blank"><button class="w3-button w3-black w3-round-large highlighter" title="Github"><img src="images/gitHub.png" alt="GitHub"></button></a></p>
                         </div>
                       <div class="spacer30"></div>
                 </div>
